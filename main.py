139ec5b5fd23f9d398ea1c93cdec9a64673b9161
DomNidy <dominicnidy3@gmail.com>
2018-11-11 20:07:06 -0500

    Added movement system

--- main.py
+++ main.py
@@ -2,16 +2,44 @@ import pygame
 import pygame,sys
 import random
 
-#Game where we have to dodge walls
-
 pygame.display.init()
-screen_width=700
-screen_height=400
+screen_width=1600
+screen_height=900
 screen=pygame.display.set_mode([screen_width,screen_height])
 pygame.display.set_caption("Turnaments")
 
-while True:
-      if(event.type==QUIT):
-            pygame.quit()
-            sys.exit
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+      pygame.time.delay(100)
+
+      for event in pygame.event.get():
+            #Quits Game
+            if event.type == pygame.QUIT:
+                  run = False
+                  
+      keys = pygame.key.get_pressed()
+
+      if keys[pygame.K_LEFT]:
+                  x -= vel
+      if keys[pygame.K_RIGHT]:
+                  x += vel
+      if keys[pygame.K_UP]:
+                  y -= vel
+      if keys[pygame.K_DOWN]:
+                  y += vel
+
+      screen.fill((0, 0, 0))
+      pygame.draw.rect(screen, (0, 0, 255), (x, y, width, height))
+      pygame.display.update()
+
+pygame.quit()
+#End                 
+            
+      
             
