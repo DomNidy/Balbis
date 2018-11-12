c4ea996414413e7c1a5ce1c8056217a4a7b7b7c1
DomNidy <dominicnidy3@gmail.com>
2018-11-11 23:44:20 -0500

    Added border and finish line. Working on collision detection

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import pygame,sys
 import random
 
 pygame.display.init()
+pygame.font.init()
 screen_width=1600
 screen_height=900
 screen=pygame.display.set_mode([screen_width,screen_height])
 pygame.display.set_caption("Turnaments")
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 5
+class Character:
+      x = 50
+      y = 50
+      width = 40
+      height = 60
+      vel = 0.3
+      area = x * y
+
+class Border:
+      x = 50
+      y = 50
+      width = 30
+      height = 60
+      area = x * y
+
+class AllObjects(Character, Border):
+      pass
 
 run = True
 while run:
-      pygame.time.delay(100)
+      pygame.time.delay(1)
 
       for event in pygame.event.get():
             #Quits Game
@@ -26,16 +39,29 @@ while run:
       keys = pygame.key.get_pressed()
 
       if keys[pygame.K_LEFT]:
-                  x -= vel
+                  Character.x -= Character.vel
+                  Character.width += 0.1
+                  Character.height += 0.1
       if keys[pygame.K_RIGHT]:
-                  x += vel
+                  Character.x += Character.vel
+                  Character.width += 0.1
+                  Character.height += 0.1
       if keys[pygame.K_UP]:
-                  y -= vel
+                  Character.y -= Character.vel
+                  Character.width += 0.1
+                  Character.height += 0.1
       if keys[pygame.K_DOWN]:
-                  y += vel
-
+                  Character.y += Character.vel
+                  Character.width += 0.1
+                  Character.height += 0.1
+      
+      Character.area = Character.width * Character.height            
+      print("Characters Area: " + str(Character.area))
+      
+                  
       screen.fill((0, 0, 0))
-      pygame.draw.rect(screen, (0, 0, 255), (x, y, width, height))
+      pygame.draw.rect(screen, (0, 0, 255), (Character.x, Character.y, Character.width, Character.height))
+      pygame.draw.rect(screen, (255, 0 ,0), (Border.x, Border.y, Border.width, Border.height))
       pygame.display.update()
 
 pygame.quit()
