d214b95ea1628172c6f4058939f93b867738af11
DomNidy <dominicnidy3@gmail.com>
2018-11-11 19:32:44 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import pygame,sys
+import random
+
+#Game where we have to dodge walls
+
+pygame.display.init()
+screen_width=700
+screen_height=400
+screen=pygame.display.set_mode([screen_width,screen_height])
+pygame.display.set_caption("Turnaments")
+
+while True:
+      if(event.type==QUIT):
+            pygame.quit()
+            sys.exit
+            
