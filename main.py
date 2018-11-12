ffa32bd319dd657bfd8461d8fa3a0aad9a2793f6
DomNidy <dominicnidy3@gmail.com>
2018-11-12 01:02:24 -0500

    Added a working hitbox for the X of the finish line

--- main.py
+++ main.py
@@ -12,20 +12,27 @@ pygame.display.set_caption("Turnaments")
 class Character:
       x = 50
       y = 50
-      width = 40
-      height = 60
+      width = 10
+      height = 10
       vel = 0.3
-      area = x * y
+      area = width * height
+      max_area = 1000000
+      score = (max_area - area) /1000
 
 class Border:
-      x = 50
-      y = 50
+      x = 800
+      y = 500
       width = 30
       height = 60
       area = x * y
 
-class AllObjects(Character, Border):
-      pass
+class FinishLine:
+      x = 1500
+      y = 0
+      width = 10
+      height = 1600
+
+zoom_data = [Character.width, Character.height, Border.width, Border.height]
 
 run = True
 while run:
@@ -54,14 +61,29 @@ while run:
                   Character.y += Character.vel
                   Character.width += 0.1
                   Character.height += 0.1
+      if keys[pygame.K_TAB]:
+            for i in zoom_data:
+                  i += 100
+            
       
       Character.area = Character.width * Character.height            
-      print("Characters Area: " + str(Character.area))
+      if keys[pygame.K_TAB]:
+
+
+            print("Characters Area: " + str(Character.area))
+            
+      Character.score = (Character.max_area - Character.area * 2) / 1000
+      if keys[pygame.K_EQUALS]:
+            print("Score: " + str(Character.score))
+
+      if round(Character.x) == FinishLine.x:
+            pygame.quit()
       
                   
       screen.fill((0, 0, 0))
       pygame.draw.rect(screen, (0, 0, 255), (Character.x, Character.y, Character.width, Character.height))
       pygame.draw.rect(screen, (255, 0 ,0), (Border.x, Border.y, Border.width, Border.height))
+      pygame.draw.rect(screen, (25, 100, 93), (FinishLine.x, FinishLine.y, FinishLine.width, FinishLine.height))
       pygame.display.update()
 
 pygame.quit()
